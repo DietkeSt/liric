701ee7b45fd03a8a002d13237ef212f58213be45
Dietke Steindel <dietke@Dietkes-MBP.station>
2023-08-29 01:26:53 +0200

    Add lyric templates into python file

---
+++ lyric_templates.py
@@ -0,0 +1,190 @@
+# Here are the lyric templates stored
+
+beach_lyrics_template = """
+♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫
+
+(Verse 1)
+
+Packed my bags, flew to the coast
+Where the sea breeze hits you the most
+Sandy shores, sun tans in sight
+In {place1} town, it's always so bright
+
+(Chorus)
+
+With {partner_name}, we hit the beach
+Salt in the air, within our reach
+{pet_name} digs a hole in the ground
+Time's slipping away, such joy we've found
+
+(Verse 2)
+
+Strolling through {place2}, what a view
+Mountains and skies, all shades of blue
+Surfing {day_activity} as the waves roll by
+With {partner_name}, we touch the sky
+
+(Chorus)
+
+{partner_name}, right by my side
+Climbing up high, feeling so wide
+{flying_animal} sings from above
+Nature's rhythm, pure peace and love
+
+(Bridge)
+
+Each moment, such a sight
+{beautiful_place} shines so bright
+{night_activity} under the moon
+No schedule, just us, it's a sweet tune
+
+(Verse 3)
+
+From {place1} to {place2}, adventure we chase
+{day_activity} and {night_activity}, at our own pace
+With {partner_name}, joy's guaranteed
+This vacation's the dream we all need
+
+(Chorus)
+
+{partner_name}, as we sail the sea
+Horizons ahead, what will they be?
+{swimming_animal} guides our way
+In this paradise, we're here to stay
+
+(Outro)
+
+As the sun sets, colors paint the sky
+{last_vacation_spot}, memories, oh so high
+A vacation we'll treasure, love so true
+{partner_name}, under the stars, just us two
+
+♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫
+
+"""
+
+love_lyrics_template = """
+♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫
+
+(Verse 1)
+
+I met you on a starry night, so bright,
+In the city of {place1}, love took flight.
+Your smile, a beacon in the city's glow,
+In the world of {place1}, you're all I'd ever know.
+
+(Chorus)
+
+With {partner_name}, my heart found its song,
+In your arms, where I truly belong.
+{pet_name} wagging its tail, oh so true,
+Time with you, like morning's first dew.
+
+(Verse 2)
+
+We danced through {place2}, hand in hand,
+A world of love, like a castle in the sand.
+{day_activity}, our laughter filled the air,
+With {partner_name}, every moment was rare.
+
+(Chorus)
+
+{partner_name}, you're my guiding star,
+Together we've journeyed, no matter how far.
+{flying_animal} in the sky, so high above,
+Our love story, an epic, a tale of true love.
+
+(Bridge)
+
+Every moment, every touch,
+{beautiful_place} where our love meant so much.
+{night_activity} under the moon's gentle grace,
+In your embrace, I've found my special place.
+
+(Verse 3)
+
+From {place1} to {place2}, our love has grown,
+Through {day_activity} and {night_activity}, we've shone.
+Hand in hand with {partner_name}, forever to be,
+This love, our treasure, for all to see.
+
+(Chorus)
+
+{partner_name}, you're my heart's desire,
+In your love, I'll never tire.
+{swimming_animal} dances in the sea,
+Our love's waves, wild and free.
+
+(Outro)
+
+As the sun sets, colors in the sky,
+{last_vacation_spot}, where our love will never die.
+A love story to cherish, so pure and true,
+{partner_name}, forever, it's me and you.
+
+♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫
+
+"""
+
+nature_lyrics_template = """
+♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫
+
+(Verse 1)
+
+I walked through the woods, so wild and free,
+Where the trees whispered secrets to me.
+In the heart of {place1}, where nature thrives,
+The world felt alive, as bees buzzed in hives.
+
+(Chorus)
+
+With {partner_name}, we hiked the trail,
+Breathing in the scents, pure and frail.
+{pet_name} chasing butterflies, what a sight!
+Time's slipping away, like day into night.
+
+(Verse 2)
+
+We ventured to {place2}, so lush and green,
+Rolling hills, the most beautiful I'd seen.
+{day_activity} with the sun in the sky so wide,
+With {partner_name}, I felt so alive inside.
+
+(Chorus)
+
+{partner_name} holding my hand so tight,
+Climbing mountains, reaching a stunning height.
+{flying_animal} sang from treetops, full of glee,
+Nature's orchestra, for you and me.
+
+(Bridge)
+
+Every moment, every sight,
+{beautiful_place} bathed in golden light.
+{night_activity} under the moon's gentle hue,
+Lost in time, with stars as our view.
+
+(Verse 3)
+
+From {place1} to {place2}, our journey unfolds,
+{day_activity} and {night_activity}, stories untold.
+Hand in hand with {partner_name}, hearts so free,
+This connection with nature, a pure jubilee.
+
+(Chorus)
+
+{partner_name}, together we explore,
+In nature's embrace, we adore.
+{swimming_animal} gliding through the stream,
+In this natural haven, we live the dream.
+
+(Outro)
+
+As the sun sets, casting colors in the sky,
+{last_vacation_spot}, where our spirits fly.
+A nature's retreat, a love that's so true,
+{partner_name}, in this world, me and you.
+
+♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫ ♩ ♪ ♬ ♫
+
+"""
